7081e44f00577ebb2bdae0c54092f620fe22f08e
SlavikChuprin <chuprin.00@yandex.ru>
2021-12-05 11:59:54 +0300

    create redux part 2

--- src/redux/actions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const addContact = createAction('contactForm/addContact');
-export const deleteContact = createAction('contactForm/deleteContact');
-export const filterContact = createAction('contactList/filterContact');
